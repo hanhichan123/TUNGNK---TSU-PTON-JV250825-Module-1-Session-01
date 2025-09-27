7bf231018774262d86761aa52c273ccfc09698dd
hanhichan123 <bokaka.95@gmail.com>
2025-09-27 21:26:04 +0900

    baitapss11-2

---
+++ baitapss11-2.js
@@ -0,0 +1,9 @@
+let textString= prompt(`What is the "official" name of JavaScript`);
+
+
+if (textString === "ECMAScript"){
+    alert("Right!")
+
+} else {
+    alert(`Didn't know? ECMAScript!`);
+}
