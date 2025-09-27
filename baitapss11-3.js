fd1df02208ce41ba773bc663dbb15c39dbb541e1
hanhichan123 <bokaka.95@gmail.com>
2025-09-27 21:26:26 +0900

    baitapss11-3

---
+++ baitapss11-3.js
@@ -0,0 +1,35 @@
+let soNumber = Number(prompt(`Nhập số thứ tự (0-9)`));
+
+
+switch (soNumber) {
+    case 1:
+        alert(`Số một`);
+        break;
+        case 2:
+        alert(`Số hai`);
+        break;
+        case 3:
+        alert(`Số ba`);
+        break;
+        case 4:
+        alert(`Số bốn`);
+        break;
+        case 5:
+        alert(`Số năm`);
+        break;
+        case 6:
+        alert(`Số sáu`);
+        break;
+        case 7:
+        alert(`Số bảy`);
+        break;
+        case 8:
+        alert(`Số tám`);
+        break;
+        case 9:
+        alert(`Số chín`);
+        break;
+        default: 
+        alert(`Số này chưa được cập nhật`);
+
+}
