28cfb4087b90db9834340989d5b24684e95400f0
hanhichan123 <bokaka.95@gmail.com>
2025-09-27 21:22:25 +0900

    baitapss10-4

---
+++ baitapss10-4.js
@@ -0,0 +1,6 @@
+let fistNumber= prompt("Fist Number");
+let secondNumber= prompt("Second number");
+
+let totalNumber = (+ fistNumber) + (+ secondNumber);
+
+alert ('Tổng hai số là:' + totalNumber);
