8f48b7552187a0ffdd45c97a0fa1bf40391d993f
hanhichan123 <bokaka.95@gmail.com>
2025-10-13 09:59:19 +0900

    baitapss17

---
+++ baitapss17.js
@@ -0,0 +1,66 @@
+// Bài tập 17-1:
+
+// let text = document.querySelector(".text");
+// let hideText = document.querySelector(".hideText");
+// let showText = document.querySelector(".showText");
+
+// hideText.onclick = function() {
+//     text.style.display = "none";
+// }
+
+// showText.onclick = function() {
+//     text.style.display = "block";
+// }
+
+// Bài tập 17-2:
+
+
+// let btn = document.querySelector(".btn");
+
+// btn.onclick = function() {
+//     document.body.classList.toggle("dark-mode");
+// };
+
+// Bài tập 17-3:
+
+// let box = document.querySelector(".open-modal");
+// let modal = document.querySelector(".div-class");
+// let closeModal = document.querySelector(".btn-close");
+
+// box.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// closeModal.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// Bài tập 17-4:
+
+// let classColor = document.querySelectorAll(".class-div");
+
+// classColor.forEach(el => {
+//     el.onmouseover = function() {
+//         let coloriD = el.getAttribute('id');
+//         document.body.style.backgroundColor = coloriD;
+//     }
+// });
+
+// Bài tập 17-5:
+
+
+
+    // document.getElementById("btn").onclick = function() {
+
+    //   let email = document.getElementById("email").value;
+    //   let password = document.getElementById("password").value;
+    //   let fixedEmail = "huanrose@gmail.com";
+    //   let fixedPassword = "123456";
+    //   if (email === fixedEmail && password === fixedPassword) {
+    //     alert("Đăng nhập thành công!");
+    //   } else {
+    //     alert("Mật khẩu hoặc email không đúng");
+    //   }
+    // }
+
+    // Bài tập 17-6:
